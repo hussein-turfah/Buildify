c8abc3a717b50ac8a5f19ef67d8ad7e67bf2408d
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-07 18:46:07 +0200

    :fire: Remove several elements for cleaner code

--- src/pages/[[...page]].jsx
+++ src/pages/[[...page]].jsx
@@ -1,74 +1,34 @@
+import Head from "next/head";
 import ResizableComponent from "../../common/resizableComponent";
 import styles from "./styles/index.module.css";
 import { useState } from "react";
+import Nav from "../../common/Nav";
+import SideNav from "../../common/sideNav";
 
 export default function Page() {
   const [children, setChildren] = useState([]);
 
-  const staticChildren = [
-    {
-      name: "Button",
-      element: <button className={styles.button}>Button</button>,
-    },
-    { name: "text", element: <p className={styles.text}>text</p> },
-    {
-      name: "image",
-      element: (
-        <div className={styles.image}>
-          <img
-            src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-            alt="Picture of the author"
-            className={styles.img}
-          />
-        </div>
-      ),
-    },
-  ];
-
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <div className={styles.sidebarHeader}>
-          <h1>Elements</h1>
-          <p>Please select elements</p>
-        </div>
-        <div className={styles.sidebarContent}>
-          {staticChildren.map((child, index) => {
+      <Head>
+        <title>Buildify</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Nav />
+      <SideNav />
+      <>
+        <div>
+          {children.map((child, index) => {
             return (
-              <div
+              <ResizableComponent
                 key={index}
-                className={styles.sidebarItem}
-                onClick={() => {
-                  setChildren([...children, child]);
-                  console.log(children);
-                }}
-              >
-                {child.element}
-              </div>
+                className={styles.resizableComponent}
+                child={child.element}
+              />
             );
           })}
-          <button
-            className={styles.publishButton}
-            onClick={() => {
-              console.log("publish");
-            }}
-          >
-            Publish Code
-          </button>
         </div>
-      </div>
-      <div>
-        {children.map((child, index) => {
-          return (
-            <ResizableComponent
-              key={index}
-              className={styles.resizableComponent}
-            >
-              {child.element}
-            </ResizableComponent>
-          );
-        })}
-      </div>
+      </>
     </div>
   );
 }
