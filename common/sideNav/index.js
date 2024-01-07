12e1fbf25614655b00bb7363e377576728e2f537
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-07 23:56:26 +0200

    :boom: Add new elements

--- common/sideNav/index.js
+++ common/sideNav/index.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
+import Selector from "../selector";
 import styles from "./styles/index.module.css";
 
-export default function SideNav() {
+export default function SideNav({ children, setChildren }) {
+  const [groupedChildren, setGroupedChildren] = useState({});
 
   const staticChildren = [
     {
       name: "Button",
       element: <button className={styles.button}>Button</button>,
+      category: "Buttons",
     },
-    { name: "text", element: <p className={styles.text}>text</p> },
+    {
+      name: "Round Button",
+      element: <button className={styles.roundButton}>Button</button>,
+      category: "Buttons",
+    },
+    {
+      name: "text",
+      element: <p className={styles.text}>text</p>,
+      category: "text",
+    },
+    {
+      name: "heading",
+      element: <h1 className={styles.heading}>heading</h1>,
+      category: "text",
+    },
     {
       name: "image",
       element: (
@@ -19,29 +37,67 @@ export default function SideNav() {
           />
         </div>
       ),
+      category: "images",
     },
   ];
+  useEffect(() => {
+    staticChildren.forEach((child) => {
+      if (groupedChildren[child.category]) {
+        groupedChildren[child.category].push(child);
+      } else {
+        groupedChildren[child.category] = [child];
+      }
+    });
+    setGroupedChildren(groupedChildren);
+  });
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarHeader}>
-        <h1>Elements</h1>
-        <p>Please select elements</p>
+        <Selector
+          children={[
+            {
+              name: "Elements",
+              selected: true,
+            },
+            {
+              name: "Layouts",
+              selected: false,
+            },
+          ]}
+        />
       </div>
       <div className={styles.sidebarContent}>
-        {staticChildren.map((child, index) => {
+        {Object.keys(groupedChildren).map((key, index) => {
           return (
-            <div
-              key={index}
-              className={styles.sidebarItem}
-              onClick={() => {
-                setChildren([...children, child]);
-                console.log(children);
-              }}
-            >
-              {child.element}
+            <div key={index} className={styles.sidebarItemCon}>
+              <h3 className={styles.sidebarCateg}>{key}</h3>
+              <div
+              className={styles.itemsContainer}
+              >
+              {groupedChildren[key].map((child, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={styles.sidebarItem}
+                    onClick={() => {
+                      setChildren([...children, child.element]);
+                    }}
+                  >
+                    <div
+                      className={styles.element}
+                    >
+                    {child.element}
+                    </div>
+                    <p className={styles.sidebarItemText}>{child.name}</p>
+                  </div>
+                );
+              })}
+            </div>
             </div>
           );
         })}
+
         <button
           className={styles.publishButton}
           onClick={() => {
