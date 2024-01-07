7fb28916619fca911de0f2b80232073d6995bd7f
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-07 14:54:44 +0200

    :boom: Add several new elements

--- src/pages/[[...page]].jsx
+++ src/pages/[[...page]].jsx
@@ -1,60 +1,23 @@
-import React from "react";
-import { useRouter } from "next/router";
-import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
-import { BuilderContent } from "@builder.io/sdk";
-import DefaultErrorPage from "next/error";
 import Head from "next/head";
+import ResizableComponent from "../../common/resizablebutton";
 
-builder.init("a777b51152364070a137f5646109462b");
-
-export const getStaticProps = async ({ params }) => {
-  const page = await builder
-    .get("page", {
-      userAttributes: {
-        urlPath:
-          "/" + (Array.isArray(params?.page) ? params.page.join("/") : ""),
-      },
-    })
-    .toPromise();
-
-  return {
-    props: {
-      page: page || null,
-    },
-    revalidate: 5,
-  };
-};
-
-export async function getStaticPaths() {
-  const pages = await builder.getAll("page", {
-    fields: "data.url",
-    options: { noTargeting: true },
-  });
-
-  return {
-    paths: pages
-      .map((page) => `${page.data?.url}`)
-      .filter((url) => url !== "/"),
-    fallback: "blocking",
-  };
-}
-
-export default function Page({ page }) {
-  const router = useRouter();
-  const isPreviewing = useIsPreviewing();
-
-  if (!page && !isPreviewing) {
-    return <DefaultErrorPage statusCode={404} />;
-  }
-
+export default function Page() {
   return (
-    <>
+    <div>
       <Head>
-        <title>{page && page.data && page.data.title}</title>
-        {console.log("page", page)}
-        <>test</>
+        <title>Next.js example</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <BuilderComponent model="page" content={page || undefined} />
-    </>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "orange",
+        }}
+      >
+        <ResizableComponent />
+      </div>
+    </div>
   );
 }
