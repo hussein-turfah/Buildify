cc40186caff0f1b9864f1b01d65143db174c5f29
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-08 01:11:16 +0200

    :fire: Remove builder io

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const withBuilderDevTools = require("@builder.io/dev-tools/next")();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
-
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+};
