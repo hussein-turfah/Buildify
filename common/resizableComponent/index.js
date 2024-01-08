4e78b597257d99e435af7436d79fa379fa7299f2
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-09 01:50:20 +0200

    :wrench: Fix several issues and clean the code

--- common/resizableComponent/index.js
+++ common/resizableComponent/index.js
@@ -1,37 +1,33 @@
-import Draggable from "react-draggable";
-import { Resizable } from "re-resizable";
 import styles from "./styles/index.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Rnd } from "react-rnd";
 
-const ResizableComponent = ({ child, setCodeToCopy }) => {
-  const [height, setHeight] = useState(100);
-  const [width, setWidth] = useState(100);
+const ResizableComponent = ({ child }) => {
+  const [fontSize, setFontSize] = useState(16);
+
+  const handleFontSizeChange = (newWidth) => {
+    const newSize = Math.floor(newWidth);
+    setFontSize(newSize);
+  };
+
   return (
     <Rnd
       default={{
         x: 0,
         y: 0,
-        // width: 320,
-        // height: 200,
+        width: 320,
+        height: 200,
       }}
-      onResizeStop={(e, direction, ref, d) => {
-        setWidth(width + d.width);
-        setHeight(height + d.height);
-        setCodeToCopy(
-          `<div style="width: ${width}px; height: ${height}px; position: absolute; top: ${ref.offsetTop}px; left: ${ref.offsetLeft}px;">${child.code}</div>`
-        );
+      onResize={(e, direction, ref, d) => {
+        handleFontSizeChange(ref.offsetWidth);
       }}
       onDragStop={(e, d) => {
-        setCodeToCopy(
-          `<div style="width: ${width}px; height: ${height}px; position: absolute; top: ${d.y}px; left: ${d.x}px;">${child.code}</div>`
-        );
+        handleDragStop(e, d);
       }}
-      // className={styles.draggable}
     >
-      {/* <div className={styles.element}> */}
-      {child.element}
-      {/* </div> */}
+      <div className={styles.element} style={{ fontSize: `${fontSize}px` }}>
+        {child.element}
+      </div>
     </Rnd>
   );
 };
