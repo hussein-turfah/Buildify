c43e2a8d5259a52059fc7442095cc5472be2f3f7
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-08 02:58:40 +0200

    :star_struck: Add new elements and components to the resizable component

--- common/resizableComponent/index.js
+++ common/resizableComponent/index.js
@@ -3,7 +3,7 @@ import { Resizable } from "re-resizable";
 import styles from "./styles/index.module.css";
 import { useState } from "react";
 
-const ResizableComponent = ({ child }) => {
+const ResizableComponent = ({ child, setCodeToCopy }) => {
   const [height, setHeight] = useState(100);
   const [width, setWidth] = useState(100);
 
@@ -16,9 +16,12 @@ const ResizableComponent = ({ child }) => {
           onResizeStop={(e, direction, ref, d) => {
             setHeight(height + d.height);
             setWidth(width + d.width);
+            setCodeToCopy(
+              `<div style="width: ${width}px; height: ${height}px;">${ref.innerHTML}</div>`
+            );
           }}
         >
-          {child}
+          {child.element}
         </Resizable>
       </Draggable>
     </div>
