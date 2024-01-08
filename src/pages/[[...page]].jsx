35f3c4b9bae15ff9765c5e547a2da0a476da9084
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-08 02:55:40 +0200

    :star_struck: Add new elements and components

--- src/pages/[[...page]].jsx
+++ src/pages/[[...page]].jsx
@@ -1,12 +1,17 @@
 import Head from "next/head";
 import ResizableComponent from "../../common/resizableComponent";
 import styles from "./styles/index.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Nav from "../../common/Nav";
 import SideNav from "../../common/sideNav";
+import { InputModal } from "../../common/inputModal";
 
 export default function Page() {
   const [children, setChildren] = useState([]);
+  const [showInputModal, setShowInputModal] = useState(false);
+  const [text, setText] = useState("");
+  const [element, setElement] = useState(null);
+  const [codeToCopy, setCodeToCopy] = useState("");
 
   return (
     <div className={styles.container}>
@@ -15,20 +20,47 @@ export default function Page() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Nav />
-      <SideNav children={children} setChildren={setChildren} />
-      <>
-        <div>
-          {children.map((child, index) => {
-            return (
-              <ResizableComponent
-                key={index}
-                className={styles.resizableComponent}
-                child={child.element}
-              />
-            );
-          })}
-        </div>
-      </>
+      <SideNav
+        children={children}
+        setChildren={setChildren}
+        showInputModal={showInputModal}
+        setShowInputModal={setShowInputModal}
+        text={text}
+        setText={setText}
+        element={element}
+        setElement={setElement}
+        codeToCopy={codeToCopy}
+      />
+      {showInputModal && (
+        <InputModal
+          text={text}
+          setText={setText}
+          showInputModal={showInputModal}
+          setShowInputModal={setShowInputModal}
+          setChildren={setChildren}
+          children={children}
+          element={element}
+          setElement={setElement}
+        />
+      )}
+      <div
+        onChange={(e, ref) => {
+          setCodeToCopy(
+            `<div style="width: ${width}px; height: ${height}px;">${ref.innerHTML}</div>`
+          );
+        }}
+      >
+        {children.map((child, index) => {
+          return (
+            <ResizableComponent
+              key={index}
+              className={styles.resizableComponent}
+              child={child}
+              setCodeToCopy={setCodeToCopy}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
