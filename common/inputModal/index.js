228140aeba786742e2794b30c4c78a07508cf6f9
Hussein Turfah <husseinturfah299@gmail.com>
2024-01-08 02:56:25 +0200

    :star_struck: Add new elements and components too the input modal

--- common/inputModal/index.js
+++ common/inputModal/index.js
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react";
 import styles from "./styles/index.module.css";
-export const InputModal = ({ text, setText, setShowModal }) => {
+export const InputModal = ({
+  showInputModal,
+  setShowInputModal,
+  setChildren,
+  children,
+  element,
+  setElement,
+}) => {
+  const [src, setSrc] = useState("");
+  const [text, setText] = useState("");
+
+  
+
+  useEffect(() => {
+    if (element.name === "Image") {
+      setSrc(URL.createObjectURL(element.src));
+    }
+  }, [element]);
+
   return (
     <div className={styles.container}>
-      <input 
+      <input
+        onBlur={() => {
+          setShowInputModal(false);
+          setText("");
+        }}
         className={styles.input}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        value={
+          element.name === "Button" ||
+          element.name === "Round Button" ||
+          element.name === "Text" ||
+          element.name === "Heading"
+            ? text
+            : src
+        }
+        onChange={(e) => {
+          if (
+            element.name === "Button" ||
+            element.name === "Round Button" ||
+            element.name === "Text" ||
+            element.name === "Heading"
+          ) {
+            setText(e.target.value);
+          } else {
+            setSrc(e.target.value);
+          }
+        }}
+        placeholder="Enter your text here and then press enter"
+        type={element.name === "Image" ? "file" : "text"}
       />
     </div>
   );
